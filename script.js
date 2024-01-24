670ad13978898b7f301f62ca21567a7b3f32b42e
Nirab Shrestha <me.nirav11@gmail.com>
2024-01-24 05:22:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([37.7, -122.4], 10);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
